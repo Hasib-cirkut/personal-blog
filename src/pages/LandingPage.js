d2dce5fe1467d759ca4a2d0da067f45408b5ec96
Hasibul Huda <hasibulhuda1@gmail.com>
2020-05-30 03:33:22 +0600

    :bug: bug fix

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Nav } from '../components/Nav';
+import BlogCard from '../components/BlogCard';
 
 function LandingPage() {
 	return (
@@ -18,11 +19,11 @@ function LandingPage() {
 				<img src="/icon_linkedin.png" alt="icon facebook" className="h-6 w-6 rounded-full mx-12" />
 			</div>
 
-			<div className="flex flex-row justify-center mt-12 text-5xl">
+			<div className="flex flex-row justify-center mt-20 text-5xl">
 				<h1>Hi</h1>
 			</div>
 
-			<div className="flex flex-row justify-center mt-12 text-lg mx-64 px-24 text-center font-light tracking-tight">
+			<div className="flex flex-row justify-center mt-6 text-lg mx-64 px-24 text-center font-light tracking-tight">
 				<p>
 					I am Hasib. Programmer, tech enthusiast, newbie blogger. Into Reactive programming, Data Science and
 					Machine learning. I am currently doing my undergrad at East West university.
@@ -31,6 +32,12 @@ function LandingPage() {
 
 			<div className="flex flex-row justify-center mt-12 text-xl font-semibold">
 				<p>Recent Blogs</p>
+			</div>
+
+			<div className="flex flex-row mt-12 justify-between px-48">
+				<BlogCard title="Programming with C" subtitle="Chapter 1" />
+				<BlogCard title="Programming with C" subtitle="Chapter 2" />
+				<BlogCard title="Programming with C" subtitle="Chapter 3" />
 			</div>
 		</div>
 	);
