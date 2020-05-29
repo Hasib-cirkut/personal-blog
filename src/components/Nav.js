20961c26cc1be971396af98f3bc01ca85a80f889
Hasibul Huda <hasibulhuda1@gmail.com>
2020-05-29 21:04:09 +0600

    :tada: first commit. added tailwind support and navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Nav = () => {
+	return (
+		<div className="flex flex-row justify-between align-bottom font-semibold text-blue-700">
+			<div className="flex flex-row justify-around">
+				<p>Home</p>
+				<p className="ml-10">Blogs</p>
+			</div>
+
+			<img src="/pp.jpg" alt="" className="rounded-full w-10 h-10" />
+
+			<div className="flex flex-row">
+				<p>About</p>
+				<p className="ml-10">Contacts</p>
+			</div>
+		</div>
+	);
+};
