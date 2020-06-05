e791fcbe0ccf4e4e9ae81c2dc2fcead070901968
Hasibul Huda <hasibulhuda1@gmail.com>
2020-06-06 05:02:33 +0600

    :lipstick: added syntax highlighting

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -8,7 +8,23 @@ import { dateConverter } from '../utils/dateConverter.js';
 function getMarkdown(body) {
 	let md = marked(body, { smartypants: true });
 
-	//console.log(md);
+	md = md.replace(/\bint\b/g, `<span style="color:red">int</span>`);
+	md = md.replace(/\bfloat\b/g, `<span style="color:red">float</span>`);
+	md = md.replace(/\bdouble\b/g, `<span style="color:red">double</span>`);
+	md = md.replace(/\bchar\b/g, `<span style="color:red">char</span>`);
+	md = md.replace(/\bfor\b/g, `<span style="color:darkviolet">for</span>`);
+	md = md.replace(/\bwhile\b/g, `<span style="color:darkviolet">while</span>`);
+
+	md = md.replace(/\breturn\b/g, `<span style="color:blue">return</span>`);
+
+	//TODO: fix #include problem
+	//md = md.replace(/^#include.*$/g, `<span style="color:green">#include</span>`);
+
+	md = md.replace(/\bprintf\b/g, `<span style="color:DeepPink">printf</span>`);
+	md = md.replace(/\bscanf\b/g, `<span style="color:DeepPink">scanf</span>`);
+	md = md.replace(/\bmain\b/g, `<span style="color:DeepPink">main</span>`);
+
+	console.log(md);
 
 	return { __html: md };
 }
@@ -38,13 +54,13 @@ const BlogPage = () => {
 	}, []);
 
 	return (
-		<div className="md:mx-64 md:mt-32 flex flex-col h-full mt-8 mx-4">
+		<div className="md:mx-64 md:mt-32 flex flex-col h-full mt-8 mx-4 pb-8">
 			<Nav />
 			<div className="mt-12 md:mt-24 md:px-24 font-mono">
 				<div className="text-2xl text-center md:text-4xl font-bold">{title}</div>
 				<div className="text-base text-center font-light mt-4">{subtitle}</div>
 				<div className="text-xl text-center font-light mt-4">{createdAt}</div>
-				<div dangerouslySetInnerHTML={getMarkdown(body)} className="mt-10" />`
+				<div dangerouslySetInnerHTML={getMarkdown(body)} className="mt-10" />
 			</div>
 		</div>
 	);
