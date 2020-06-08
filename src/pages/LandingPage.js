b75ec001df4bca195f8f10996396f8f304303ae1
Hasibul Huda <hasibulhuda1@gmail.com>
2020-06-08 20:51:39 +0600

    :lipstick: blog tag ui updated v2

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -16,16 +16,22 @@ function LandingPage() {
 				<div className="flex flex-row justify-center mt-24 text-lg font-normal">
 					<p>find me on</p>
 				</div>
-				<div className={'flex flex-row justify-center mt-4 py-2 rounded-sm bg-gray-100 mx-4 md:mx-48'}>
+				<div
+					className={
+						'flex flex-row justify-around mt-4 py-2 rounded-sm border-b-4 border-pink-700 bg-gray-100 mx-4 md:mx-64'
+					}
+				>
 					<a href="https://www.facebook.com/adib.hasib.7">
-						<img src="/icon_facebook.png" alt="icon facebook" className="h-8 w-8 rounded-full mx-12" />
+						<img src="/icon_facebook.png" alt="icon facebook" className="h-8 w-8 rounded-full " />
 					</a>
 
 					<a href="https://github.com/Hasib-cirkut">
 						<img src="/icon_github.png" alt="icon facebook" className="h-8 w-8 rounded-full" />
 					</a>
 
-					<img src="/icon_linkedin.png" alt="icon facebook" className="h-8 w-8 rounded-full mx-12" />
+					<a href="https://twitter.com/HudaHasibul">
+						<img src="/icon_twitter.png" alt="icon facebook" className="h-8 w-8 rounded-full " />
+					</a>
 				</div>
 
 				<div className="flex flex-row justify-center mt-20 text-5xl">
@@ -35,7 +41,8 @@ function LandingPage() {
 				<div className="flex flex-row justify-center mt-6 text-lg md:mx-64 md:px-24 text-center font-light tracking-tight">
 					<p>
 						I am Hasib. Programmer, tech enthusiast, newbie blogger. Into Reactive programming, Data Science
-						and Machine learning. I am currently doing my undergrad at East West university.
+						and Machine learning. I am currently doing my undergrad at East West university where I'm also
+						serving as an undergraduate teaching assistant.
 					</p>
 				</div>
 
